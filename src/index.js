acf78a170ac42feafa3445a057204fcea5f95e90
Dmitry Aksenov <aksbum@gmail.com>
2023-05-11 13:41:34 +0300

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const allGame = (gameRules, userNumber) => {
   console.log(gameRules);
   for (let i = 0; i < 3; i += 1) {
     const [question, correctAnswer] = userNumber();
-    console.log(`Question:${question}`);
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== String(correctAnswer)) {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
